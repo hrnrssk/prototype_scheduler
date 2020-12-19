189d393156469d46bc4120cb1cbe7a3548e9ee50
hironori sasaki <hrnr.ssk@gmail.com>
2020-12-19 22:07:41 +0900

    マイページに担当工程登録機能を追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def edit
+    # @processings = @user.working_processings
+    @user.workings.build
+
+    # workings = Working.where(user_id: current_user.id).pluck(:processing_id)  # ログイン中のユーザーのprocessing_idカラムを取得
+    # @working_list = Processing.find(workings)     # processingsテーブルから、登録済み工程のレコードを取得
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "ユーザー情報を編集しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :email, workings_attributes:[:id, :processing_id])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
