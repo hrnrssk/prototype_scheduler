b6723dcd59d784fe514dc271c69b79266b1bd864
hironori sasaki <hrnr.ssk@gmail.com>
2021-01-11 22:11:00 +0900

    edit画面でもAjaxでスケジュールが表示されるよう修正

--- app/models/processing.rb
+++ app/models/processing.rb
@@ -4,7 +4,8 @@ class Processing < ApplicationRecord
   belongs_to :equipment
   has_many :workings, dependent: :destroy
   has_many :working_users, through: :workings, source: :user
-  has_many :flows, dependent: :destroy
+  # has_many :flows, dependent: :destroy
+  has_many :flows
   has_many :flow_prototypes, through: :flows, source: :prototype
   accepts_nested_attributes_for :flows, allow_destroy: true
 end
