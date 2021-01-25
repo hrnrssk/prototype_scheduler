c2eceb15d83ecbb0a644af65f93135a346fb40e7
hironori sasaki <hrnr.ssk@gmail.com>
2021-01-25 23:20:42 +0900

    エラー時の処理を修正

--- app/helpers/equipments_helper.rb
+++ app/helpers/equipments_helper.rb
@@ -1,6 +1,6 @@
 module EquipmentsHelper
   def equipment_choose_new_or_edit
-    if action_name == 'new' || action_name == 'create'
+    if action_name == 'new' || action_name == 'create' || action_name == 'confirm'
       confirm_equipments_path
     elsif action_name == 'edit'
       equipment_path
--- app/helpers/processings_helper.rb
+++ app/helpers/processings_helper.rb
@@ -1,9 +1,9 @@
 module ProcessingsHelper
   def processing_choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
-        confirm_processings_path
+      confirm_processings_path
     elsif action_name == 'edit'
-        processing_path
+      processing_path
     end
   end
 end
