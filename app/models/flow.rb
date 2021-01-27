b99197fe7067cf5508671d0690f4150cd81238ef
hironori sasaki <hrnr.ssk@gmail.com>
2021-01-27 23:07:10 +0900

    納期が過去の日付で登録できないようにバリデーションを設定した

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -1,24 +1,24 @@
 class Flow < ApplicationRecord
   validates :number, numericality: { only_integer: true }, presence: true
-  validate :date_before_scheduled_start
-  validate :date_before_scheduled_end
-  validate :date_before_end
+  validate :scheduled_start_date
+  validate :scheduled_end_date
+  validate :end_date
   belongs_to :prototype, optional: true
   belongs_to :processing, optional: true
   has_many :assignings, dependent: :destroy
   has_many :assigning_users, through: :assignings, source: :user
 
-  def date_before_scheduled_start
+  def scheduled_start_date
     return if scheduled_starting_time.blank?
     errors.add(:scheduled_starting_time, "は今日以降の日付を選択してください") if scheduled_starting_time < Date.today
   end
 
-  def date_before_scheduled_end
+  def scheduled_end_date
     return if scheduled_ending_time.blank?
     errors.add(:scheduled_ending_time, "は今日以降の日付を選択してください") if scheduled_ending_time < Date.today
   end
 
-  def date_before_end
+  def end_date
     return if ending_time.blank?
     errors.add(:ending_time, "は今日以前の日付を選択してください") if ending_time > Date.today
   end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,11 @@ class Prototype < ApplicationRecord
   validates :customer, length: { maximum: 20 }, presence: true
   validates :name, length: { maximum: 30 }, presence: true
   validates :delivery_date, presence: true
+  validate :delivery_date_before_today
   has_many :flows, dependent: :destroy
   has_many :flow_processings, through: :flows, source: :processing
+
+  def delivery_date_before_today
+    errors.add(:delivery_date, "は今日以降の日付を選択してください") if delivery_date < Date.today
+  end
 end
