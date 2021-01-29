0736daa6d6dec6161bbabab1707591b6c6201d4b
hironori sasaki <hrnr.ssk@gmail.com>
2021-01-29 21:24:21 +0900

    日付のバリデーションテストを修正

--- spec/models/flow_spec.rb
+++ spec/models/flow_spec.rb
@@ -3,7 +3,12 @@ RSpec.describe Flow, type: :model do
   # 工程NOがあれば有効な状態であること
   it "is valid with a number" do
     flow = Flow.new(
-        number: '1',)
+        number: '1',
+        processing_id: '1',
+        user_id: '1',
+        scheduled_starting_time: '2100-01-20 00:00',
+        scheduled_ending_time: '2100-01-21 00:00',
+        ending_time: '2021-01-20 00:00')
     expect(flow).to be_valid
   end
   # 工程NOがなければ無効な状態であること
@@ -22,25 +27,6 @@ RSpec.describe Flow, type: :model do
   # 開始予定と終了予定が本日以前であれば無効な状態であること
   # flow.valid?メソッドを実行し、失敗すると、flowインスタンスの中にエラーメッセージが入ること
   it "is invalid without integrity of date" do
-    Equipment.create(
-      id: '1',
-      name: '装置',
-      comment: 'ロボット')
-    User.create(
-      id: '1',
-      name: '山田',
-      email: 'example@example.com',
-      password: 'password',
-      confirmed_at: Time.now)
-    Processing.create(
-      id: '1',
-      equipment_id: '1',
-      name: '工程',
-      comment: 'プロセス',
-      time_required: '00:30')
-    Working.create(
-      user_id: '1',
-      processing_id: '1')
     flow = Flow.new(
       number: '1',
       processing_id: '1',
